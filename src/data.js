94ddb631b9b46d68e7991988947c33d6c8956a3a
Linsy <Linsy.Joyner@gmail.com>
2017-01-10 15:08:50 -0500

    updated data and finished adding components

--- src/data.js
+++ src/data.js
@@ -1,29 +1,89 @@
 const game = {
-    users: {
-      2332535: {
-        name: 'Linsy Joyner',
-        username: 'linsydeanna',
-        isAvailable: true
+  "FirebaseGenNode": {
+    "invitations": {
+      "autoGenKeyInvite": {
+        "sender": "GitHubUserName",
+        "receiver": "GitHubUserName"
       }
     },
-    games: {
-      9032840: {
-        player1: 'linsydeanna',
-        player2: 'ross',
-        player1Score: 0,
-        player2Score: 0,
-        questions: {
-          1: {
-            question: 'Complete the classic line: “That’s no moon; it’s a...”',
-            correctAnswer: 'Space station',
-            isAnswered: true,
-            answeredBy: 'linsydeanna',
-            playerAnswer: 'Droid',
-            isCorrect: false
+    "users": {
+      "autoGenUserKey": {
+        "name": "Linsy Joyner",
+        "username": "GitHubUserName",
+        "available": "true",
+        "invitedby": "player",
+        "gameswon": 4
+      }
+    },
+    "games": {
+      "autoGenGameKey":{
+        "player1": "GitHubUserName",
+        "player2": "GitHubUserName",
+      },
+      "game rounds": {
+        "autoGenKey1": {
+          "question": "Complete the classic line: That's no moon; it's a...",
+          "correctAnswer": "Spacestation",
+          "answers": {
+            "player1": {
+              "answer": "", "timeAnswered": "time", "isCorrect": true
+            },
+            "player2": {
+              "answer": "", "timeAnswered": "time", "isCorrect": true
+            }
           }
+        },
+        "autoGenKey2": {
+          "question": "Complete the classic line: That's no moon; it's a...",
+          "correctAnswer": "Spacestation",
+          "answers": {
+            "player1": {
+              "answer": "", "timeAnswered": "time", "isCorrect": true
+            },
+            "player2": {
+              "answer": "", "timeAnswered": "time", "isCorrect": true
+            }
+          }
+        },
+      },
+      "autoGenKey3": {
+        "question":"Complete the classic line: That's no moon; it's a...",
+        "correctAnswer":"Spacestation",
+        "answers": {
+          "player1": {
+            "answer": "", "timeAnswered": "time", "isCorrect": true
+          },
+          "player2": {
+            "answer": "", "timeAnswered": "time", "isCorrect": true
+          }
+        }
+      },
+    },
+    "autoGenKey4": {
+      "question": "Complete the classic line: That's no moon; it's a...",
+      "correctAnswer": "Spacestation",
+      "answers": {
+        "player1": {
+          "answer": "", "timeAnswered": "time", "isCorrect": true
+        },
+        "player2": {
+          "answer": "", "timeAnswered": "time", "isCorrect": true
+        }
+      }
+    },
+    "autoGenKey5": {
+      "question": "Complete the classic line: That's no moon; it'sa...",
+      "correctAnswer":"Spacestation",
+      "answers": {
+        "player1": {
+          "answer": "", "timeAnswered": "time", "isCorrect": true
+        },
+        "player2": {
+          "answer": "", "timeAnswered": "time", "isCorrect": true
         }
       }
     }
   }
+}
 
 export default game;
--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -1,6 +1,7 @@
 import game from '../data';
 
 function gameReducer(state = game, action) {
+  console.log(state)
   return state;
 }
 

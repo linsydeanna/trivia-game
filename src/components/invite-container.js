0c6625a5771a348722f93eb8aee11032ec40537a
Linsy <Linsy.Joyner@gmail.com>
2017-01-13 01:31:56 -0500

    creating invitations from firebase data

--- src/components/invite-container.js
+++ src/components/invite-container.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import base from '../base'
+import { Well } from 'react-bootstrap';
 
 class InviteContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      invitations: []
+    }
+  }
+
+  componentDidMount() {
+    this.rebaseRef = base.syncState('invitations', {
+      context: this,
+      state: 'invitations',
+      asArray: true
+    })
+  }
+
   render() {
     return (
       <div className="HomeLeft">
-        InviteContainer
+        <div className="InviteBox">
+          <p>Invites Sent</p>
+          <div>
+            {this.state.invitations.map((invitation, index) => {
+              if (invitation.sender === "Linsy Joyner") {
+                return <Well key={index}>Your invite to {invitation.receiver} is pending.</Well>
+              }
+            })}
+          </div>
+        </div>
+        <div className="InviteBox">
+          <p>Invites Received</p>
+        </div>
       </div>
     );
   }
